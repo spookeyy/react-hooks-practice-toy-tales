8961601c4c5012baed53bd19cb779fe38916b9f5
spookeyy <pangasmeshack@gmail.com>
2024-05-02 13:29:02 +0300

    axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -7,18 +8,32 @@ import ToyContainer from "./ToyContainer";
 function App() {
   const [showForm, setShowForm] = useState(false);
 
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/toys")
+      .then((response) => setToys(response.data))
+      .catch((error) => console.log(error));
+  }, [])
+
   function handleClick() {
     setShowForm((showForm) => !showForm);
+  }
+
+  function addNewToy(newToy) {
+    setToys((toys) => [...toys, newToy]); // spread operator(... creates a copy of an array)
+
   }
 
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm addToy={addNewToy}/> : null} {/* ternary operator */}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys}/>
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+function ToyForm({addToy}) {
 
-function ToyForm() {
+const [formData, setFormData] = useState({
+  name: "",
+  image: "",
+});
+
+
+function handleChange(event) {
+  setFormData({
+    ...formData,
+    [event.target.name]: event.target.value, // spread operator
+    // [event.target.name]: is the key of the object in the useState function, event.target.value is the value of the key
+  });
+}
+
+//handle submit
+function handleSubmit(e){
+  e.preventDefault();
+  const newToy = {
+    ...formData, likes: 0
+  };
+  
+  axios
+    .post("http://localhost:3001/toys", newToy)
+    .then((response) => addToy(response.data));
+  }
+
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
+          onChange={handleChange}
+          value={formData.name}
           placeholder="Enter a toy's name..."
           className="input-text"
         />
@@ -15,6 +45,8 @@ function ToyForm() {
         <input
           type="text"
           name="image"
+          onChange={handleChange}
+          value={formData.image}
           placeholder="Enter a toy's image URL..."
           className="input-text"
         />
@@ -25,6 +57,9 @@ function ToyForm() {
           value="Create New Toy"
           className="submit"
         />
+        <button className="cancel-btn" onClick={() => addToy(formData)}>
+          Cancel
+        </button>
       </form>
     </div>
   );
